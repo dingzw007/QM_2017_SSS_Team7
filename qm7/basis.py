8a3e36589ee230926b1d6d2fd4c02265e2619633
ssx9f <ssx9f@mst.edu>
2017-07-26 13:24:11 -0500

    Library for basis set

---
+++ qm7/basis.py
@@ -0,0 +1,22 @@
+"""
+Building the library of the basis functions.
+"""
+import psi4
+
+
+def get_basis(mol, basis_set):
+    """
+    Builds mints for a molecule and its basis sets
+    """
+    bas = psi4.core.BasisSet.build(mol, target=basis_set)
+    bas.print_out()
+
+    #Build a MintsHelper
+    mints = psi4.core.MintsHelper(bas)
+    nbf = mints.nbf()
+
+    if (nbf > 100):
+	raise Exception("MOre than 100 basis functions")
+
+
+    return mints
