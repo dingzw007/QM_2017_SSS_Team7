2b39158857fc7fdf0da5abb88284546ffff62c94
srinivasmushnoori <srinivas.21689@gmail.com>
2017-07-26 16:42:03 -0400

    conjugate gradient solver

---
+++ tests/test_conjugategradient.py
@@ -0,0 +1,13 @@
+import numpy as np
+import qm7.conjugategradient as cg
+
+def test_cg():
+
+    A = np.array([[1, 2],
+                 [2, 1]])
+    b = np.array([3, 3])
+    x_init = np.array([0.01, 0.01])
+    ##assert cg.conjugate_gradient(A, b, x_init) == (1, 1)
+    
+    error = cg.conjugate_gradient(A, b, x_init) - np.array([1, 1])
+    assert np.linalg.norm(error) < 1e-8	
